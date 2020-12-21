61f1868445517f15272cf09f0f27bd19e23a2b8d
Lucas <lucas.braga@msn.com>
2020-12-21 00:03:17 -0300

    exercício 22

---
+++ ex022.py
@@ -0,0 +1,17 @@
+# @author: lucvsbraga
+
+'''
+Crie um programa que leia o nome completo de uma pessoa e mostre:
+
+> O nome com todas as letras maaiúsculas e minúsculas.
+> Quantas letras ao todo (sem considerar espaços).
+> Quantas letras tem o primeiro nooe.
+'''
+
+nome = str(input('Digite o nome completo: ')).strip()
+print(f'UPPER: {nome.upper()}')
+print(f'LOWER: {nome.lower()}')
+primeiro_nome = nome.find(' ')
+total = len(nome) - nome.count(' ')
+print(f'Número de letras primeiro nome: {primeiro_nome}')
+print(f'Total de letras: {total}')
