a7b3e0f301d3661beab475893e5e7cadc3db025c
Lucvs <lucas.braga@msn.com>
2021-02-11 17:45:33 -0300

    silent

---
+++ ex090.py
@@ -0,0 +1,16 @@
+# @author: lucvsbraga
+import os
+
+aluno = {}
+aluno['Nome'] = input('Nome: ')
+aluno['Média'] = float(input('Média: '))
+if aluno['Média'] < 5:
+    aluno['Situação'] = 'Reprovado'
+elif aluno['Média'] < 7:
+    aluno['Situação'] = 'Recuperação'
+else:
+    aluno['Situação'] = 'Aprovado'
+    
+os.system('clear')
+for k, v in aluno.items():
+    print(f'{k}: {v}')
